77009bf37ed36aaf649bdd3002b096e04790d8da
Bhagi <puppyreddy1997@gmail.com>
2020-11-28 19:56:46 +0530

    [Bhagyalaxmi]ADDED:Added code to analyse the Happy and sad moods

---
+++ MoodAnalyseTest/unitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyZerProblem;
+
+namespace MoodAnalyseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void givenSadMood_Should_Return_Sad()
+        {
+            MoodAnalyser analyser = new MoodAnalyser("i am  in Sad Mood");
+            string mood = analyser.analyseMood();
+            Assert.AreEqual("SAD", mood);
+        }
+        [TestMethod]
+        public void givenHappyMood_Should_Return_Happy()
+        {
+            MoodAnalyser analyser = new MoodAnalyser("i am  in Happy Mood");
+            string mood = analyser.analyseMood();
+            Assert.AreEqual("HAPPY", mood);
+        }
+    }
+}
